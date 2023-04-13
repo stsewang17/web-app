d587736ef1bf2cb463a69bfd3e58c8149ea5585d
Wes <wes@not.real>
2023-04-13 15:44:14 -0500

    added cloudbuild yaml fil25e

---
+++ env-build/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "gcs" {
+    bucket = "sopa-tf-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = "ageless-rig-375816"
+  zone    = "us-central1-c"
+  region  = "us-central1"
+}
+
